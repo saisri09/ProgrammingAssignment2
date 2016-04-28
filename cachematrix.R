5241e4da4586d678379a80cb1edbf96e7afb9524
saisri09 <saisri.malempati@gmail.com>
2016-04-28 15:17:06 +0530

    updated Cache.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,18 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+inver = NULL
+  set = function(y) {
+    # use `<<-` to assign a value to an object in an environment 
+    # different from the current environment. 
+    x <<- y
+    inver <<- NULL
+  }
+  get = function() x
+  setinv = function(inverse) inver <<- inverse 
+  getinv = function() inver
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
+
 }
 
 
